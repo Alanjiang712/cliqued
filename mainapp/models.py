f3d8426ba19531ad56ef263d1610df327320aa74
Alanjiang712 <g1832954352@gmail.com>
2024-02-02 11:38:36 +0800

    update models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User, AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
-import uuid
-
-# Create your models here.
 
 class CustomUser(AbstractUser):
     # 用户头像
-    useravatar = models.ImageField(upload_to='useravatars/', null=True, blank=True)
-    
+    useravatar = models.ImageField(
+        upload_to='useravatars/', null=True, blank=True)
+
     # UID UUIDField
     # uid = models.UUIDField(default=uuid.uuid4, unique=True)
 
+
 class Habit(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='habits')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name='habits')
     habit_title = models.CharField(max_length=255)
     habit_description = models.TextField()
     start_date = models.DateField()
@@ -28,3 +27,32 @@ class Habit(models.Model):
     def __str__(self):
         return self.habit_title
 
+
+class HabitTracking(models.Model):
+    habit = models.ForeignKey('Habit', on_delete=models.CASCADE)
+    date = models.DateField()
+    STATUS_CHOICES = (
+        ('Completed', 'Completed'),
+        ('Missed', 'Missed'),
+    )
+    picture = models.ImageField(
+        upload_to='habit_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.habit.habit_title} on {self.date}"
+
+
+class Match(models.Model):
+    user1 = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='matches_user1')
+    user2 = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='matches_user2')
+    match_date = models.DateField()
+    STATUS_CHOICES = (
+        ('Matching', 'Matching'),
+        ('Matched', 'Matched'),
+        ('Match_off', 'Match_off'),
+    )
+
+    def __str__(self):
+        return f"Match between {self.user1.username} and {self.user2.username} on {self.match_date}"
