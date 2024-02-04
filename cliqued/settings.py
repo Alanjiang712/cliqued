247f3d9c3930fb6ef2bee79ad6b82455714c26e1
Alanjiang712 <g1832954352@gmail.com>
2024-02-04 22:05:05 +0800

    fix SECRET_KEY

--- cliqued/settings.py
+++ cliqued/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-4og#kgqj4g*6@6pe%7lkrv@l#(5m@escui2j!glyn@erzx!hnb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') 
