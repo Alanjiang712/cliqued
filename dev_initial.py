057ecad54bb6a341968564b02b6ef369db51a015
Alanjiang712 <g1832954352@gmail.com>
2024-02-04 22:52:53 +0800

    change user settings

--- dev_initial.py
+++ dev_initial.py
@@ -1,9 +1,8 @@
 import os
-import shutil
 import django
 from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cliqued.settings')
 django.setup()
 
 from django.contrib.auth.models import User
@@ -17,6 +16,6 @@ def create_superuser(username, email, password):
     print('Username: %s' % username, 'Password: %s' % password)
   else:
     print(f"Superuser {username} already exists!")
-    
+
 load_dotenv() 
-create_superuser('admin', 'admin@example.com', os.getenv('DJANGO_ADMIN_PASSWORD'))
+create_superuser('alan', 'email@email.com', os.getenv('DJANGO_ADMIN_PASSWORD'))
